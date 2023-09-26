defbbcd96ba884253b2f9c1f39f3285b22c45f9d
fairygarbage <111027159+fairygarbage@users.noreply.github.com>
2023-09-26 14:48:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Importa las bibliotecas necesarias
 import streamlit as st  
 import pandas as pd    
 import numpy as np     
@@ -7,207 +6,69 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline          
 from sklearn.impute import SimpleImputer        
 from sklearn.preprocessing import FunctionTransformer, StandardScaler, OneHotEncoder  
-from sklearn.base import BaseEstimator, TransformerMixin  
-
-
-def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
-    os.makedirs(housing_path, exist_ok=True)
-    tgz_path = os.path.join(housing_path, "housing.tgz")
-    urllib.request.urlretrieve(housing_url, tgz_path)
-    housing_tgz = tarfile.open(tgz_path)
-    housing_tgz.extractall(path=housing_path)
-    housing_tgz.close()
-
-fetch_housing_data(HOUSING_URL, HOUSING_PATH)
-
-
-def load_housing_data(housing_path=HOUSING_PATH):
-    csv_path = os.path.join(housing_path, 'housing.csv')
-    return pd.read_csv(csv_path)
-
-load_housing_data(HOUSING_PATH)
-
-
-def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
-
-fetch_housing_data(HOUSING_URL, HOUSING_PATH)
-housing = load_housing_data(HOUSING_PATH)
-train_set, test_set = split_train_test(housing, 0.2)
-#print(len(housing))
-#print(len(train_set))
-
-train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
-#len(housing)
-#len(train_set)
-
-housing["income_cat"] = pd.cut(housing["median_income"],
-                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
-                               labels=[1, 2, 3, 4, 5])
-
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in split.split(housing, housing["income_cat"]):
-    strat_train_set = housing.loc[train_index]
-    strat_test_set = housing.loc[test_index]
-
-for set_ in (strat_train_set, strat_test_set):
-    set_.drop("income_cat", axis=1, inplace=True)
-
-housing = strat_train_set.copy()
-housing.plot(kind="scatter", x="longitude", y="latitude")
-
-housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
-
-housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
-             s=housing["population"]/100, label="population", figsize=(10,7),
-             c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
-             )
-plt.legend()
-
-#corr_matrix = housing.corr()
-#corr_matrix["median_house_value"].sort_values(ascending=False)
-
-attributes = ["median_house_value", "median_income", "total_rooms",
-              "housing_median_age"]
-
-scatter_matrix(housing[attributes], figsize=(12,8))
-
-housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
-housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
-housing["population_per_household"] = housing["population"]/housing["households"]
-
-corr_matrix = housing.corr()
-corr_matrix["median_house_value"].sort_values(ascending=False)
-
-
-housing = strat_train_set.drop("median_house_value", axis=1)
-housing_labels = strat_train_set["median_house_value"].copy()
-
-
-
-
-imputer = SimpleImputer(strategy="median")
-housing_num = housing.drop("ocean_proximity", axis=1)
-imputer.fit(housing_num)
-
-X = imputer.transform(housing_num)
-housing_tr = pd.DataFrame(X, columns=housing_num.columns,
-                          index=housing_num.index)
-housing_cat = housing[["ocean_proximity"]]
-housing_cat.head(10)
-
-
-ordinal_encoder = OrdinalEncoder()
-housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
-housing_cat_encoded[:10]
-
-
-ordinal_encoder.categories_
-
-cat_encoder = OneHotEncoder()
-housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
-housing_cat_1hot
-
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin 
 
 rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
 
-class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
-  def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
-    self.add_bedrooms_per_room = add_bedrooms_per_room
-  def fit(self, X, y=None):
-    return self # nothing else to do
-  def transform(self,X):
-    rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
-    population_per_household = X[:, population_ix] / X[:, households_ix]
-    if self.add_bedrooms_per_room:
-      bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
-      return np.c_[X, rooms_per_household, population_per_household,
-                   bedrooms_per_room]
-    else:
-      return np.c_[X, rooms_per_household, population_per_household]
-attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
-housing_extra_attribs = attr_adder.transform(housing.values)
+
+class CombinedAttributesAdder(BaseEstimator,TransformerMixin):
+    def __init__(self,add_bedrooms_per_room = True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    
+    def fit(self,X, y = None):
+        return self
+
+    def transform(self, X, y=None):
+            rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+            population_per_household = X[:, population_ix] / X[:, household_ix]
+            if self.add_bedrooms_per_room:
+                    bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+                    return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+            else:
+                return np.c_[X, rooms_per_household, population_per_household]
 
 
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+def transform(self, X, y=None):
+            rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+            population_per_household = X[:, population_ix] / X[:, household_ix]
+            if self.add_bedrooms_per_room:
+                    bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+                    return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+            else:
+                return np.c_[X, rooms_per_household, population_per_household]
 
-num_pipeline = Pipeline([
-    ('imputer', SimpleImputer(strategy="median")),
-    ('attribs_adder', CombinedAttributesAdder()),
-    ('std_scaler', StandardScaler()),
-])
+def predicts(data):
+    model = joblib.load("final_model.pkl")
+    pipeline = joblib.load("full_pipeline.pkl")
+    data = pipeline.transform(data)
+    
+    return model.predict(data)
+    
 
-housing_num_tr = num_pipeline.fit_transform(housing_num)
-     
+st.header('Model Prediction house price value in California')
 
-from sklearn.compose import ColumnTransformer
+longitude = st.number_input("Longitude", max_value=0.0)
+latitude = st.number_input("Latitude", min_value=1.0)
+age = st.number_input("Housing Median Age", min_value=1.0)
+total_rooms = st.number_input("Total rooms", min_value=1.0)
+total_bedrooms = st.number_input("Total bedrooms", min_value=1.0)
+population = st.number_input("Population", min_value=1.0)
+households = st.number_input("Households", min_value=1.0)
+income = st.number_input("Median income", min_value=1.0)
+ocean_proximity = st.selectbox("Ocean Proximity: ",['<1H OCEAN', 'NEAR BAY', 'NEAR OCEAN', 'INLAND', 'ISLAND'])
 
-num_attribs = list(housing_num)
-cat_attribs = ["ocean_proximity"]
+ if st.button('Search prediction'):
+        data = pd.DataFrame({
+            'longitude': [longitude],
+            'latitude': [latitude],
+            'housing_median_age': [housing_median_age],
+            'total_rooms': [total_rooms],
+            'total_bedrooms': [total_bedrooms],
+            'population': [population],
+            'households': [households],
+            'median_income': [median_income],
+            'ocean_proximity': [ocean_proximity]}
+        )
 
-full_pipeline = ColumnTransformer([
-    ("num", num_pipeline, num_attribs),
-    ("cat", OneHotEncoder(), cat_attribs),
-])
-
-housing_prepared = full_pipeline.fit_transform(housing)
-
-def display_scores(scores):
-  print("Scores", scores)
-  print("Mean", scores.mean())
-  print("standard deviation", scores.std())
-
-
-     
-
-#Linear Regression
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-
-lin_reg = LinearRegression()
-lin_reg.fit(housing_prepared, housing_labels)
-housing_predictions = lin_reg.predict (housing_prepared)
-lin_mse = mean_squared_error (housing_labels, housing_predictions)
-lin_rmse = np.sqrt(lin_mse)
-lin_rmse
-
-
-some_data = housing.iloc[:5]
-some_labels = housing_labels.iloc[:5]
-some_data_prepared = full_pipeline.transform(some_data)
-
-
-#Random forest
-from sklearn.ensemble import RandomForestRegressor
-
-forest_reg = RandomForestRegressor()
-forest_reg.fit(housing_prepared, housing_labels)
-housing_predictions = forest_reg.predict(housing_prepared)
-forest_mse = mean_squared_error (housing_labels, housing_predictions)
-forest_rmse = np.sqrt (forest_mse)
-forest_rmse
-
-
-from sklearn.model_selection import GridSearchCV
-
-param_grid = [
-    {'n_estimators': [3,10,30],'max_features': [2,4,6,8]},
-    {'bootstrap': [False], 'n_estimators' : [3,10], 'max_features': [2,3,4]},
-]
-
-forest_reg = RandomForestRegressor()
-
-grid_search_RF = GridSearchCV(forest_reg, param_grid, cv=5,
-                           scoring='neg_mean_squared_error',
-                           return_train_score=True)
-
-grid_search_RF.fit(housing_prepared, housing_labels)
-
-final_model = grid_search_RF.best_estimator_
-
+result = predicts(data)
+st.write("The predicted value is {:.1f} USD".format(result[0]))
